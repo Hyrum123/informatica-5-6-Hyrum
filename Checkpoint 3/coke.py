a69321efc3d979c798b59a6f222b7069052cfeb2
Hyrum Whetten <jeromehyhy@gmail.com>
2025-08-28 20:06:02 +0000

    can i keep it

--- Checkpoint 3/coke.py
+++ Checkpoint 3/coke.py
@@ -1,11 +1,23 @@
 def main():
     name = input("What's you name? ")
-    coins = int(input("Pay with 25, 10, or 5: "))
-    while total <= 50:
-        if coins == 25 or coins == 10 or coins == 5:
-            total = total + coins
-        else:
+    price = 50
+    total_paid = 0
+    vending_machine(price, total_paid, name)
+
+
+def vending_machine(price, total_paid, name):
+    while price > 0:
+        print(f"Amount due: {price}")
+        pay = int(input("Insert Coin: "))
+
+        if pay == 25 or pay == 10 or pay == 5:
+            total_paid = total_paid + pay
+            price = price - pay
+        else: 
             print("Not a coin")
+    if total_paid > 50:
+        print(f"Thanks! Here's your coke {name}")
+        print(f"Here's your change: {total_paid - 50}")
 
 
-main()
+main()eep it
