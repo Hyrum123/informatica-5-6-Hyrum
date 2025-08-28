2a9fb10fb3281b1241ec8082a5978ace574c5387
Hyrum Whetten <jeromehyhy@gmail.com>
2025-08-28 16:41:06 +0000

    there we go brother

--- Checkpoint 3/jumps.py
+++ Checkpoint 3/jumps.py
@@ -1,12 +1,23 @@
-num = int(input("Enter a number my guy: "))
-if num == str:
-    print("no")
-step = int(input("Enter a step size my guy: "))
-start = step
+def main():
+    num = input("Enter a number my guy: ")
+    num = test(num)
+    step = input("Enter a step size my guy: ")
+    step = test(step)
+    num = int(num)
+    step = int(step)
+    math_loop(num, step)
 
+def test(a):
+    while not a.isdigit():
+        a = input("Enter an integer fool: ")
+    return a
 
-while start < num:
-    if start == 0:
-        step = int(input("Enter a new step size, not zero: "))
-    print(start)
-    start += step
+def math_loop(number, steps):
+    start = steps
+    while start < number:
+        if start == 0:
+            steps = int(input("Enter a new step size, not zero: "))
+        print(start)
+        start += steps
+
+main()
