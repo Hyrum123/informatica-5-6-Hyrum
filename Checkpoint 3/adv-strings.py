878e40e6093e687d8fd09f331c855e89c73fd69c
Hyrum Whetten <jeromehyhy@gmail.com>
2025-09-01 19:37:44 +0000

    ta bueno

---
+++ Checkpoint 3/adv-strings.py
@@ -0,0 +1,19 @@
+#String Length
+input_string = input("Please type a message: ")
+
+# print(input_string)
+# print("-" * len(input_string))
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[3])
+
+#Example
+# i = len(input_string) - 1
+# while i >= 0:
+#     print(input_string[i])
+#     i -= 1
+
+print("h" in input_string)
+print("j" in input_string)
+print("ello" in input_string)
