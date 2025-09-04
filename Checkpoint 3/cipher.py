8035b5a54678066c9611f115947b699594804676
Hyrum Whetten <jeromehyhy@gmail.com>
2025-09-04 16:24:04 +0000

    thank you profe

--- Checkpoint 3/cipher.py
+++ Checkpoint 3/cipher.py
@@ -1,7 +1,6 @@
 def main():
     text = input("Enter a secret message: ").strip().lower()
-    output = encode_message(text)
-    print(output)
+    encode_message(text)
 
 def encode_message(text):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -9,9 +8,12 @@ def encode_message(text):
     letter_loc = 0
     while letter_loc < len(text):
         if text[letter_loc] in alphabet:
-                alphabet_loc = alphabet.find(text[letter_loc])
-                print(cipher[alphabet_loc])
+            alphabet_loc = alphabet.find(text[letter_loc])
+            print(cipher[alphabet_loc], end="")
+        else:
+            print(text[letter_loc], end="")
         letter_loc += 1
+    print("")
 
 main()
 
