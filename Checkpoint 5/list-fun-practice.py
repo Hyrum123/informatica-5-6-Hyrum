4e43135a56e81f16d1142be2528cb57f8e9c135c
Hyrum Whetten <jeromehyhy@gmail.com>
2025-09-11 17:30:01 +0000

    is goin good

---
+++ Checkpoint 5/list-fun-practice.py
@@ -0,0 +1,23 @@
+def main():
+    my_list = [1, 2, 3, 4, 5, 5, 5, 6, 7, 7]
+    print(length(my_list))
+    print(mean(my_list))
+    print(range_of_list(my_list))
+
+
+def length(lists):
+    return len(list(lists))
+
+def mean(lists):
+    return sum(lists) / len(lists)
+
+def range_of_list(lists):
+    return max(lists) - min(lists)
+
+def enter():
+    lists = []
+    while True: 
+        
+
+
+main()
