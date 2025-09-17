914b1d2f1755bec277d81986c927ccf06553e5d9
Hyrum Whetten <jeromehyhy@gmail.com>
2025-09-17 20:21:04 +0000

    its good

---
+++ Checkpoint 6/for-intro.py
@@ -0,0 +1,15 @@
+doctrine = ["Faith", "Repentance", "Baptism", "Holy Ghost", "Endure to the End"]
+
+# i = 0
+# while i < len(doctrine):
+#     print(doctrine[i])
+#     i += 1
+
+for principle in doctrine:
+    print(principle)
+
+school_objective = ["Deepen conversion to Jesus Christ and His restored Gospel", "Qualify for the blessings of the Temple", "Prepare for missionary and church service", "Build the kingdom of God"]
+
+for objective in school_objective:
+    print(objective)
+    print("⭐")
