3d201a298a1a870779b488d5bdd2b16deca54168
Hyrum Whetten <jeromehyhy@gmail.com>
2025-10-07 16:22:18 +0000

    coolio

--- Checkpoint 8/scrabble.py
+++ Checkpoint 8/scrabble.py
@@ -9,6 +9,7 @@ alphabet = {
 
 
 letters = []
+
 for letter in range(13):
     letters.append((random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])))
 print(f"Your letters given are:", end=" ")
