a84f8c8322d4b8aa7cf06ec97088f9de3c85400e
Hyrum Whetten <jeromehyhy@gmail.com>
2025-10-23 17:23:15 +0000

    commited

---
+++ Checkpoint 10/exceptions.py
@@ -0,0 +1,30 @@
+# SyntaxError
+# print("Hello, world)
+
+# ValueError
+# try:
+#     x = int(input("What's x? "))
+#     print(f"x is equal to {x}")
+# except ValueError:
+#     print("x is not a number")
+
+# ZeroDivisionError
+# def spam(divide_by):
+#     try:
+#         return 42 / divide_by
+#     except ZeroDivisionError:
+#         print("dont divide by zero")
+
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+while True:
+    try:
+        x = int(input("What's x "))
+    except ValueError:
+        print("x is not a number")
+    else:
+        break
+print(f"x is equal to {x}")
