e67920af18569dbe1e96181a0df9a223747eec52
Hyrum Whetten <jeromehyhy@gmail.com>
2025-10-23 19:58:21 +0000

    yeah man

--- Checkpoint 10/exceptions.py
+++ Checkpoint 10/exceptions.py
@@ -20,11 +20,26 @@
 # print(spam(0))
 # print(spam(1))
 
-while True:
-    try:
-        x = int(input("What's x "))
-    except ValueError:
-        print("x is not a number")
-    else:
-        break
-print(f"x is equal to {x}")
+# while True:
+#     try:
+#         x = int(input("What's x "))
+#     except ValueError:
+#         print("x is not a number")
+#     else:
+#         break
+# print(f"x is equal to {x}")
+
+def read_small_integer():
+    while True:
+        try:
+            input_str = input("Please type an integer: ")
+            number = int(input_str)
+            if number < 100 and number >= 0:
+                return number
+        except ValueError:
+            pass
+        print("This input is invalid")
+    
+        
+number = read_small_integer()
+print(number, "to the power of three is", number**3)
