f7f649709928d17ed0bf775b15703344d6e79b55
Hyrum Whetten <jeromehyhy@gmail.com>
2025-11-04 16:21:49 +0000

    simon

--- Checkpoint 11/libraries.py
+++ Checkpoint 11/libraries.py
@@ -29,7 +29,7 @@ except IndexError:
 import sys
 import cowsay
 try:
-    cowsay.cow("Hello my name is", sys.argv[1])
+    cowsay.cow("Hello my name is " + sys.argv[1])
 except IndexError:
     print("Too few arguments")
     
