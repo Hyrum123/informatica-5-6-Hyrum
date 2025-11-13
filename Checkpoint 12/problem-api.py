f6c4c324989da585f4d76770367068c66beeb63e
Hyrum Whetten <jeromehyhy@gmail.com>
2025-11-13 20:20:00 +0000

    gottyyyy

--- Checkpoint 12/problem-api.py
+++ Checkpoint 12/problem-api.py
@@ -1,13 +1,25 @@
 import requests
 
-type_operation = input("What type of operation you want to do?, we have \nSimplify, Factor, Derive, Integrate, Cosine, Sine, Tangent\n").lower()
+correct_names = {"simplify": "simplify", "factor": "factor", "derive": "derive", "integrate": "integrate", "zeroes": "zeroes", 
+                 "tangent": "tangent", "area": "area", "cosine": "cos", "sine": "sin", "tangent": "tan", "inverse cosine": "arccos", 
+                 "inverse sine": "arcsin", "inverse tangent": "arctan", "absolute value": "abs", "logarithm": "log"}
 
-expression = input("Awsome, now enter the expression you want to solve\n")
-# equation = input("Enter your operation and your expression (seperated by a comma): ").lower().split(",")
-# for element in equation:
-#     print(element)
+while True:
+    type_operation = input("What type of operation you want to do?, we have \nSimplify, Factor, Derive, Integrate, Cosine, Sine, Tangent, etc.\n").lower()
 
-if type_operation == "derive" 
+    expression = input("Awesome, now enter the expression you want to solve\n")
+    # equation = input("Enter your operation and your expression (seperated by a comma): ").lower().split(",")
+    # # for element in equation:
+    # #     print(element)
 
-answer = requests.get("https://newton.vercel.app/api/v2/"+ type_operation + "/" + expression)
-print(f"To {answer.json()["operation"]} {answer.json()["expression"]}, the answer is: {answer.json()["result"]}")
+    verbs = { "derive" : "derivative", "simplify" : "simplification", "factor" : "factorization", "integrate" : "integral" }
+    try:
+        answer = requests.get("https://newton.vercel.app/api/v2/"+ correct_names[type_operation] + "/" + expression)
+        break
+    except KeyError:
+        print("This ain't an operation, please try again")
+
+try:
+    print(f"The {verbs[answer.json()["operation"]]} of {answer.json()["expression"]} is: {answer.json()["result"]}")
+except KeyError:
+    print(f"The {answer.json()["operation"]} of {answer.json()["expression"]} is: {answer.json()["result"]}")
