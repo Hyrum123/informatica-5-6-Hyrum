ad8f28c1e5f93a37f5528e38641409f20c9da31c
Hyrum Whetten <jeromehyhy@gmail.com>
2025-11-13 16:31:51 +0000

    ok

--- Checkpoint 12/bitcoin.py
+++ Checkpoint 12/bitcoin.py
@@ -11,3 +11,7 @@ while True:
 API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
 
 response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+bitcoin_price = float(response.json()["data"]["priceUsd"])
+total = round(coins * bitcoin_price, 2)
+
+print(f"Your total will be ${total:,.2f}")
